b22ce08aa73db674f22b3a9a377a0abfb01b1788
Katelin <willikat036@alpinesd.org>
2024-10-22 09:45:55 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,10 @@
-#while summation code following directions from Readme
+user_num = 4
+looped_times = 1
+total = 0
+
+while looped_times <= user_num:
+    print(looped_times)
+    total = total + looped_times
+    looped_times = looped_times + 1  
+  
+print("Total sum:", total) #while summation code following directions from Readme
